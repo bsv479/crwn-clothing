802a6f09f15945f5cfe5da32573ae70be9307ce4
BSV479 <dzham47@gmail.com>
2021-01-06 19:00:16 +0100

    Created cart-item component and cart dropdown.

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -6,7 +6,7 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
   if(isExisting) {
     return cartItems.map(cartItem =>
       (cartItem.id === cartItemToAdd.id) ?
-        {cartItem, quantity: cartItem.quantity + 1} : cartItem
+        {...cartItem, quantity: cartItem.quantity + 1} : cartItem
     )
   }
   return [...cartItems, {...cartItemToAdd, quantity: 1}]
