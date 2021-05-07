2c67158eb428b957c977774126f3116968ec03a7
BSV479 <dzham47@gmail.com>
2021-05-08 00:11:18 +0200

    Adjusted pay button styles

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 import axios from 'axios';
 import StripeCheckout from 'react-stripe-checkout';
+import './stripe-button.styles.scss';
 
 
-const StripeCheckoutButton = ({price}) => {
+const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_51IHosCDRMabatm23LGU2B22i4lBgBoE2EbWx6urODYsn1q3npUH2gi5kw3ru8QJkRQEhh0vbVDAmYKshjzVxXHpv00jyZcPpeO';
 
@@ -28,19 +29,21 @@ const StripeCheckoutButton = ({price}) => {
       });
   };
 
-  return(
-    <StripeCheckout
-      label='Pay Now'
-      name='CRWN Clothing Ltd.'
-      billingAddress
-      shippingAddress
-      image='https://svgshare.com/i/CUz.svg'
-      description={`Your total is $${price}`}
-      amount={priceForStripe}
-      panelLabel='Pay Now'
-      token={onToken}
-      stripeKey={publishableKey}
-    />
+  return (
+    <div className='stripe-btn'>
+      <StripeCheckout
+        label='Pay Now'
+        name='CRWN Clothing Ltd.'
+        billingAddress
+        shippingAddress
+        image='https://svgshare.com/i/CUz.svg'
+        description={`Your total is $${price}`}
+        amount={priceForStripe}
+        panelLabel='Pay Now'
+        token={onToken}
+        stripeKey={publishableKey}
+      />
+    </div>
   )
 };
 
